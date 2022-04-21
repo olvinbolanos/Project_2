f88eb1cc5dbbd7ea57f71a75932454d6489235e6
olvin bolanos <olvin.bolanos@yahoo.com>
2022-04-20 17:37:35 -0700

    made the split array in the for loop work

--- sort.cpp
+++ sort.cpp
@@ -23,44 +23,35 @@ int main (void) {
     std::cout << "\n Split # " << split_num++ << ": ";
     std::cout << "\nElement number " << myvector[i] << " ";
   }
-
-  printf("\nElements of the array::");
-
-  /*
-  for(i=0;i<sz;i++)
-  {
-    printf("\nElement number %d::%d",i+1,myvector[i]);
-  }
-  */
   // using default comparison (operator <):
-  /*
+
   std::sort (myvector.begin(), myvector.begin()+5);
   // using object as comp
   std::sort (myvector.begin() + 5, myvector.end());
-  */
+
   // using function as comp
   //std::sort (myvector.begin()+5, myvector.end());             // 12 32 45 71(26 33 53 80)
 
   // print out content:
+  //for (auto elem:myvector){}
+  /*
   std::cout << "myvector contains:";
   for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
-    std::cout << "\n " << *it;
+    std::cout << " " << *it;
   std::cout << '\n';
+  */
   // print out content:
-  /*
+
   int counter = 0;
-  int split_num = 1;
-  int split_num2 = 2;
+  int split_num2 = 1;
   for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it) {
       if(counter % 5 == 0) {
-        std::cout << "\n Split # " << split_num++ << ": ";
-      } else if(counter % 10 == 0) {
-        std::cout << "\n Split # " << split_num++ << ": ";
+        std::cout << "\n Split # " << split_num2++ << ": ";
       }
     std::cout << *it << " ";
     counter++;
   }
   std::cout << '\n';
-*/
+
   return 0;
 }
